86f14759dc2381acb8fb338f54898879b9830231
XzoRit <xzorit@hotmail.com>
2019-09-15 10:19:11 +0200

    create more useful description on how to use the console application

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/program_options.hpp>
 #include <lager/event_loop/manual.hpp>
+#include <lager/event_loop/queue.hpp>
 #include <lager/store.hpp>
 #include <lib/lib.hpp>
 #include <iostream>
@@ -23,18 +24,29 @@ inline const auto render = [](const auto& m)
 
 int main(int ac, char* av[])
 {
-  std::cout << "Hello app\n";
+  std::cout << "loger app\n";
 
   try
   {
-    po::options_description desc("Allowed options");
-    desc.add_options()("help", "produce help message");
+    po::options_description desc("options for lager app");
+    desc.add_options()
+        ("help", "produce help message")
+        ("+", "increments counter by 1")
+        ("-", "decrements counter by 1")
+        ("=", "resets counter to 0")
+        ("q", "quits application")
+        ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(ac, av, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help")) { std::cout << desc << "\n"; }
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 0;
+    }
+    std::cout << desc << "\n";
   }
   catch (std::exception& e)
   {
@@ -46,11 +58,16 @@ int main(int ac, char* av[])
     std::cerr << "Exception of unknown type!\n";
   }
 
+  auto evt_q = lager::queue_event_loop{};
   auto store = lager::make_store<xzr::lib::action>(
       xzr::lib::model{},
       xzr::lib::update,
       render,
-      lager::with_manual_event_loop{});
+      lager::with_manual_event_loop{}
+      // events will be queued and only executed
+      // after q.step() has been called
+      // lager::with_queue_event_loop{evt_q}
+      );
 
   auto c = char{};
   while(std::cin >> c)
@@ -60,6 +77,7 @@ int main(int ac, char* av[])
       {
           store.dispatch(*act);
       }
+      evt_q.step();
   }
 
   return 0;
