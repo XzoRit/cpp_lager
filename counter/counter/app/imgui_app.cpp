f5dc2b3b25d16f426805832db2040bece757ca4b
XzoRit <xzorit@hotmail.com>
2020-04-01 16:59:50 +0200

    throw on error of creating gui context

--- counter/counter/app/imgui_app.cpp
+++ counter/counter/app/imgui_app.cpp
@@ -19,14 +19,14 @@
 
 namespace
 {
+using namespace std::string_literals;
 struct sdl
 {
     sdl()
     {
         if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
-            throw std::runtime_error{std::string{"SDL_Init failed "} + SDL_GetError()};
+            throw std::runtime_error{"SDL_Init failed "s + SDL_GetError()};
     }
-
     ~sdl() noexcept
     {
         SDL_Quit();
@@ -44,15 +44,19 @@ struct sdl_window
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
         SDL_WindowFlags window_flags =
             (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-        handle =
-            SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+        handle = SDL_CreateWindow("counter imgui app",
+                                  SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED,
+                                  1280,
+                                  720,
+                                  window_flags);
+        if (!handle)
+            throw std::runtime_error{"SDL_CreateWindow failed "s + SDL_GetError()};
     }
-
     ~sdl_window() noexcept
     {
         SDL_DestroyWindow(handle);
     }
-
     SDL_Window* handle{nullptr};
 };
 
@@ -60,10 +64,12 @@ struct sdl_gl_context
 {
     explicit sdl_gl_context(SDL_Window* window) : gl_context{SDL_GL_CreateContext(window)}
     {
+        if (!gl_context)
+            throw std::runtime_error{"SDL_GL_CreateContext failed "s + SDL_GetError()};
+
         SDL_GL_MakeCurrent(window, gl_context);
         SDL_GL_SetSwapInterval(1);
     }
-
     ~sdl_gl_context() noexcept
     {
         SDL_GL_DeleteContext(gl_context);
@@ -74,7 +80,7 @@ struct sdl_gl_context
 
 struct imgui_context
 {
-    imgui_context(SDL_Window* window, SDL_GLContext gl_context) : win{window}
+    imgui_context(SDL_Window* window, SDL_GLContext gl_context) : win{window}, gl_ctx{gl_context}
     {
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
@@ -84,59 +90,66 @@ struct imgui_context
         ImGui_ImplSDL2_InitForOpenGL(win, gl_context);
         ImGui_ImplOpenGL2_Init();
     }
-
     void new_frame()
     {
         ImGui_ImplOpenGL2_NewFrame();
         ImGui_ImplSDL2_NewFrame(win);
         ImGui::NewFrame();
     }
-
     void render()
     {
         ImGui::Render();
+        SDL_GL_MakeCurrent(win, gl_ctx);
+        const auto size{ImGui::GetIO().DisplaySize};
+        glViewport(0, 0, (int)size.x, (int)size.y);
 
-        ImGuiIO& io = ImGui::GetIO();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-
-        const ImVec4 back_ground_color{0.5f, 0.5, 0.5, 0.5f};
-        glClearColor(back_ground_color.x, back_ground_color.y, back_ground_color.z, back_ground_color.w);
+        glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
         SDL_GL_SwapWindow(win);
     }
-
     SDL_Window* win{nullptr};
+    SDL_GLContext gl_ctx{};
 };
 } // namespace
 int main()
 {
-    [[maybe_unused]] sdl sdl_context{};
-    sdl_window window{};
-    sdl_gl_context gl_context{window.handle};
-    imgui_context gui_context{window.handle, gl_context.gl_context};
+    try
+    {
+        [[maybe_unused]] sdl sdl_context{};
+        sdl_window window{};
+        sdl_gl_context gl_context{window.handle};
+        imgui_context gui_context{window.handle, gl_context.gl_context};
 
-    auto loop = lager::sdl_event_loop{};
-    auto store = lager::make_store<xzr::counter::action::action>(xzr::counter::model::model{},
-                                                                 xzr::counter::model::update,
-                                                                 lager::with_sdl_event_loop{loop});
+        auto loop = lager::sdl_event_loop{};
+        auto store = lager::make_store<xzr::counter::action::action>(xzr::counter::model::model{},
+                                                                     xzr::counter::model::update,
+                                                                     lager::with_sdl_event_loop{loop});
 
-    loop.run(
-        [&](const SDL_Event& ev) {
-            ImGui_ImplSDL2_ProcessEvent(&ev);
-            return (ev.type != SDL_QUIT);
-        },
-        [&](auto&&) {
-            gui_context.new_frame();
-            {
-                xzr::counter::view::imgui::draw(store, store.get());
-                ImGui::ShowDemoWindow();
-            }
-            gui_context.render();
-            return true;
-        });
-
+        loop.run(
+            [&](const SDL_Event& ev) {
+                ImGui_ImplSDL2_ProcessEvent(&ev);
+                return (ev.type != SDL_QUIT);
+            },
+            [&](auto&&) {
+                gui_context.new_frame();
+                {
+                    xzr::counter::view::imgui::draw(store, store.get());
+                    ImGui::ShowDemoWindow();
+                }
+                gui_context.render();
+                return true;
+            });
+    }
+    catch (const std::exception& e)
+    {
+        return 1;
+    }
+    catch (...)
+    {
+        return 2;
+    }
     return 0;
 }
